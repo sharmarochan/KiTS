62842a866ab1cc3be30010db583454460c24e37b
sharmarochan <sharmarochan@gmail.com>
2019-04-03 12:22:18 +0000

    code clean and push

--- kits.py
+++ kits.py
@@ -36,22 +36,16 @@ import cv2
 
 
 
-data_set = r"E:\kits19\data"
+data_set = r"E:\kits19\data"    #data set path
+   
+all_patients =  next(os.walk(data_set))[1]      #get the name of the folder_names  
 
 
-#get the name of the folder_names        
-all_patients =  next(os.walk(data_set))[1]
-
-imaging_arr = []
-segmentation_arr = []
-
-IMG_PIC_SIZE = 512
+IMG_PIC_SIZE = 512    #to contol the size of each slice
 
 
-train=[]
-target = []
-slices_images_imageFile = []
-slices_images_segFile = []
+slices_images_imageFile = []    #slices of the images that will be used for trainig
+slices_images_segFile = []      #slices of the images that will be used for TARGET
 
 
 
@@ -61,74 +55,59 @@ max_arr=[]
 min_arr = []
     
 
-for patient in all_patients[:1]:
+for patient in all_patients[:2]:
     semi_full_path =  os.path.join(data_set,patient)
 
     files_per_patient =  next(os.walk(semi_full_path))[2]
     for file in files_per_patient:
         file_type, _, _= file.split(".")
+        full_path =  os.path.join(semi_full_path,file)
         
-        if file_type == "segmentation":
-            full_path =  os.path.join(semi_full_path,file)
-            segmentation_arr.append(full_path)
-
+        if file_type == "segmentation":     #segmentation file
             
-            #segmentation file
-            img_seg = nib.load(full_path).get_data()
+            img_seg = nib.load(full_path).get_data()   #load nifti file as ndarrray
             
             
-            pic_having_kidey = []
-            pic_having_cancer = []
+            pic_having_kidey = []           #slices having kidney not cancer
+            pic_having_cancer = []          #slices having kidney and cancer section
+            
+            
             for slice_num_seg in range(img_seg.shape[0]):
-#                count = count+1
-                slice_seg = img_seg[slice_num_seg, :, :]
-                max_val = slice_seg.max()
-                if (max_val == 1):
+                slice_seg = img_seg[slice_num_seg, :, :]        #slices the 3d image based on depth
+                
+                max_val = slice_seg.max()                       #extract the max value based on the label of the pixels
+                if (max_val == 1):                              # detecting pic having kidney only
                     pic_having_kidey.append(slice_num_seg)
                     
-                if (max_val == 2):
+                if (max_val == 2):                              # detecting pic having cancer and kideny section
                     pic_having_cancer.append(slice_num_seg)
                 
                 im_path = os.path.join(tmp_save_dir,(patient+'_'+file_type+'_slice_'+str(slice_num_seg)+".png"))
-                plt.imsave(im_path,slice_seg , cmap='gray')
+                plt.imsave(im_path,slice_seg , cmap='gray')     #save image to directroy
 
-                slices_images_segFile.append(slice_seg)
-                
-
-            #append to the array
-            target.append(img_seg)
-            
-            
-            
-        if file_type == "imaging":
-            
-            #append to the array
-            imaging_arr.append(full_path)
+                slices_images_segFile.append(slice_seg)         #append to targrt array
         
-            full_path =  os.path.join(semi_full_path,file)
-            img_kidney = nib.load(full_path).get_data()
-
-            
-            train.append(img_kidney)
+        
+        if file_type == "imaging":                              #for full MRI image
+            img_kidney = nib.load(full_path).get_data()         #get nd array from nifti
 
      
             for slice_num_kidney in range(img_kidney.shape[0]):
                 slice_kidney = img_kidney[slice_num_kidney, :, :]
                 im_path = os.path.join(tmp_save_dir , (patient+'_'+file_type+'_slice_'+str(slice_num_kidney)+".png"))
-                plt.imsave(im_path , slice_kidney,cmap = 'gray')
+                plt.imsave(im_path , slice_kidney,cmap = 'gray')        #save to directory
 
-                slices_images_imageFile.append(slice_kidney)
+                slices_images_imageFile.append(slice_kidney)   #for training 
             
             
             
         
-slices_images_imageFile_arr = np.array(slices_images_imageFile)
+Image_data = np.array(slices_images_imageFile)          #convert list to ndarray
 
-slices_images_segFile_arr = np.array(slices_images_segFile)
-            
-print("[INFO] data matrix: {:.2f}MB".format(slices_images_imageFile_arr.nbytes / (1024 * 1000.0)))  
-            
-print("[INFO] data matrix: {:.2f}MB".format(slices_images_segFile_arr.nbytes / (1024 * 1000.0)))      
+Label_data = np.array(slices_images_segFile) 
+                                                    #to check the size of traing and testing array
+print("Label data : {:.2f}MB  \
+      Image data  :{:.2f}MB ".format(Label_data.nbytes / (1024 * 1000.0) , Image_data.nbytes / (1024 * 1000.0)))      
             
           
 
