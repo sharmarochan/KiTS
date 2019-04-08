b166570e089d248c3dc330a2c43d60a443205f41
sharmarochan <sharmarochan@gmail.com>
2019-04-08 12:57:42 +0000

    temp_code add

--- temp_code.py
+++ temp_code.py
@@ -298,7 +298,26 @@ from sklearn.model_selection import train_test_split
 
 
 
+import numpy as np 
+a = np.array([[1,2,3],[4,5,6]]) 
 
+print ('First array:' )
+print (a.shape) 
+print ('\n'  )
+
+print ('Append elements to array:' )
+
+b = np.append(a, [7,8,9]) 
+print (b.shape)
+print ('\n'  )
+
+print ('Append elements along axis 0:' )
+c = np.append(a, [[7,8,9]],axis = 0)
+print(c.shape)
+print ('\n'  )
+
+print ('Append elements along axis 1:' )
+print (np.append(a, [[5,5,5],[7,8,9]],axis = 1))
 
 
 
